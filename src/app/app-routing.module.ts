53416fdacd7172295ee31eea755fed7566daf510
Himanshu Gohil <himanshugohil234@gmail.com>
2022-12-24 15:04:14 +0530

    added chat header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './chats/home/home.component';
 import { StartPageComponent } from './start-page/start-page.component';
 
-const routes: Routes = [{ path: '', component: StartPageComponent }];
+const routes: Routes = [
+  { path: '', component: StartPageComponent },
+  { path: 'home', component: HomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StartPageComponent } from './start-page/start-page.component';
+import { HomeComponent } from './chats/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StartPageComponent
+    StartPageComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
