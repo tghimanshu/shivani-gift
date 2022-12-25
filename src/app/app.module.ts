7e90ba1814e018c731dda228d837cacf7038066c
Himanshu Gohil <himanshugohil234@gmail.com>
2022-12-25 11:48:21 +0530

    created chat screen file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,9 +11,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatIconModule } from '@angular/material/icon';
 import { AllChatsComponent } from './chats/all-chats/all-chats.component';
+import { ChatScreenComponent } from './chat/chat-screen/chat-screen.component';
 
 @NgModule({
-  declarations: [AppComponent, StartPageComponent, HomeComponent, AllChatsComponent],
+  declarations: [AppComponent, StartPageComponent, HomeComponent, AllChatsComponent, ChatScreenComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
