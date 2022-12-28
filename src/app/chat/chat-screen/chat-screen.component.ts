27d49f084ec94910474487b04a8dbf1ade81591c
Himanshu Gohil <himanshugohil234@gmail.com>
2022-12-28 16:19:19 +0530

    added chats service and integrated with all chats and initiated with chat screen

--- src/app/chat/chat-screen/chat-screen.component.ts
+++ src/app/chat/chat-screen/chat-screen.component.ts
@@ -1,10 +1,39 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ChatsService } from 'src/app/service/chats.service';
 
 @Component({
   selector: 'app-chat-screen',
   templateUrl: './chat-screen.component.html',
-  styleUrls: ['./chat-screen.component.scss']
+  styleUrls: ['./chat-screen.component.scss'],
 })
 export class ChatScreenComponent {
+  chat!: {
+    id: number;
+    profilePic: string;
+    name: string;
+    msg: string;
+    msgs: string[];
+    answer: string;
+  };
+  constructor(
+    private chatsService: ChatsService,
+    private route: ActivatedRoute
+  ) {}
 
+  ngOnInit() {
+    this.chatsService
+      .getChat(this.route.snapshot.params['id'] as number)
+      .subscribe((val) => {
+        if (val) {
+          this.chat = val;
+        }
+      });
+    this.route.params.subscribe((val) => {
+      this.chatsService.getChat(val['id'] as number).subscribe((val2) => {
+        if (val2) this.chat = val2;
+      });
+    });
+  }
 }
